fab59d73526ae910bed25f9d0409b221ffef8696
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-09-20 11:38:05 +0200

    primeros componentes, diseño de cada uno de ellos y la carpeta order

---
+++ app/order/layout.tsx
@@ -0,0 +1,23 @@
+
+
+//children sera el contenido de cada una de las paginas hijo
+
+import OrderSideBar from "@/components/order/OrderSideBar";
+import OrderSummary from "@/components/order/OrderSummary";
+
+//indica cada uno de los segmentos hijos que se va a inyectar a traves de children
+export default function RootLayout({children, }: Readonly<{children: React.ReactNode}>) {
+    return (
+        <>
+            <div className="md:flex">
+                <OrderSideBar />
+
+                <main className="md:flex-1 md:h-screen md:overflow-y-scroll p-5">
+                    {children}
+                </main>
+
+                <OrderSummary />
+            </div>
+        </>
+    )
+}
