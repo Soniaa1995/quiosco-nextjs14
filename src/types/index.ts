c1aae96107d37e429964833a5fe30dc34cea36d9
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-09-25 10:51:14 +0200

    administramos los productos en su propia página, dándole funcionalidad y creando nuevos componentes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,14 @@
-import { Product } from "@prisma/client";
+import { Order, OrderProducts, Product } from "@prisma/client";
 
 //con pick hacemos la seleccion con lo que queremos trabajar es decir, del Producto, cogemos solo el id, name y price
 
 export type OrderItem = Pick<Product, 'id' | 'name' | 'price'> & { 
     quantity: number
     subtotal: number
+}
+
+export type OrderWithProducts = Order & { // el & para agregar más atributos
+    orderProducts: (OrderProducts & {
+        product: Product
+    })[]
 }
